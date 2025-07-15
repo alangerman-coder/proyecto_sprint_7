d1bc9f961471c52c8907f3a90685cd9073153602
alangerman-coder <alan_a_fi@hotmail.com>
2025-07-15 11:15:59 -0600

    kernel restart

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import pandas as pd
 # Título de la app
 st.title("Análisis de Vehiculos")
 # Cargar los datos
+
+
 @st.cache_data
 def cargar_datos():
     # Simulación de carga, reemplaza esto con tu CSV o fuente real
     df = pd.read_csv("vehicles_us.csv")  # Asegúrate de tener este archivo
     return df
+
+
 df = cargar_datos()
 # Mostrar tabla
 st.subheader("Vista previa del dataset")
