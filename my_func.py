5947359bb9a0858bc661d69aca8f60d9e7b25609
alangerman-coder <alan_a_fi@hotmail.com>
2025-07-15 10:39:34 -0600

    version

--- my_func.py
+++ my_func.py
@@ -1,10 +1,12 @@
 # my_func.py
-product_price = 12.0 #variable global
-product_qty = 5      #variable global
+product_price = 12.0  # variable global
+product_qty = 5  # variable global
+
 
 def total_price():
-    product_price = 11.0 #variable local
-    product_qty = 4      #variable local
+    product_price = 11.0  # variable local
+    product_qty = 4  # variable local
     print(f'Precio total: {product_price * product_qty}')
+
 
 total_price()
