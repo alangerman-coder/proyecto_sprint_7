2bed56c3f9a4afc36c66713ab805223184868d06
alangerman-coder <alan_a_fi@hotmail.com>
2025-07-15 12:55:58 -0600

    agregando diagrma de dispersion

--- app.py
+++ app.py
@@ -19,11 +19,6 @@ df = cargar_datos()
 st.subheader("Vista previa del dataset")
 st.dataframe(df.head(50))  # Muestra los primeros 50 por rendimiento
 # Histograma
-# crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
-
-if build_histogram:  # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
 # Leer los datos del archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
 
@@ -46,22 +41,29 @@ if hist_button:
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
     st.plotly_chart(fig, use_container_width=True)
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
 # Grafico de dispersion
 build_histogram = st.checkbox('Construir un diagrama de dispersion')
 
 if build_histogram:  # si la casilla de verificación está seleccionada
     st.write(
         'Construir un diagrama de dispersio para las columnas Relación entre Odómetro y Precio')
-# Leer los datos del archivo CSV
-car_data = pd.read_csv('vehicles_us.csv')
+    # Leer los datos del archivo CSV
+    car_data = pd.read_csv('vehicles_us.csv')
 
-# Crear un scatter plot utilizando plotly.graph_objects
-# Se crea una figura vacía y luego se añade un rastro de scatter
-fig = go.Figure(data=[go.Scatter(x=car_data['odometer'],
-                y=car_data['price'], mode='markers')])
+    # Crear un scatter plot utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de scatter
+    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'],
+                                     y=car_data['price'], mode='markers')])
 
-# Opcional: Puedes añadir un título al gráfico si lo deseas
-fig.update_layout(title_text='Relación entre Odómetro y Precio')
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
 
-# Mostrar el gráfico Plotly
-fig.show()
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
